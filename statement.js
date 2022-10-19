50ed2dbb847bccf3bc8500823962769bf342ca69
WillyLiu <willywcliu@kknow.com>
2022-10-20 00:16:20 +0800

    Rename parameter

--- statement.js
+++ statement.js
@@ -21,11 +21,11 @@ const statement = (invoice, plays) => {
     result += `You earned ${volumeCredits} credits\n`
     return result
 
-    function volumeCreditsFor(perf) {
-        let volumeCredits = 0
-        volumeCredits += Math.max(perf.audience - 30, 0)
-        if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
-        return volumeCredits
+    function volumeCreditsFor(aPerformance) {
+        let result = 0
+        result += Math.max(aPerformance.audience - 30, 0)
+        if (playFor(aPerformance).type === 'comedy') result += Math.floor(aPerformance.audience / 5)
+        return result
     }
 
     function playFor(perf) {
