2050123fb3fb376b0d90d939e37e6a3e4a51d66a
WillyLiu <willywcliu@kknow.com>
2022-10-20 00:31:47 +0800

    加了一個物件來當”中間資料結構“

--- statement.js
+++ statement.js
@@ -1,7 +1,8 @@
 const statement = (invoice, plays) => {
-    return renderPlainText(invoice, plays)
+    const statementData = {}
+    return renderPlainText(statementData, invoice, plays)
 }
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`
 
     for (let perf of invoice.performances) {
