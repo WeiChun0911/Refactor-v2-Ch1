23e2eeb936b268ed24aad60cbf6f7a667e0002fd
WillyLiu <willywcliu@kknow.com>
2022-10-20 00:21:12 +0800

    Move divided by 100 into usd function

--- statement.js
+++ statement.js
@@ -7,10 +7,10 @@ const statement = (invoice, plays) => {
         volumeCredits += volumeCreditsFor(perf)
 
         // 印出這筆訂單
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
         totalAmount += amountFor(perf)
     }
-    result += `Amount owed is ${usd(totalAmount / 100)}\n`
+    result += `Amount owed is ${usd(totalAmount)}\n`
     result += `You earned ${volumeCredits} credits\n`
     return result
 
@@ -19,7 +19,7 @@ const statement = (invoice, plays) => {
             style: 'currency',
             currency: 'USD',
             minimumFractionDigits: 2,
-        }).format(aNumber)
+        }).format(aNumber / 100)
     }
 
     function volumeCreditsFor(aPerformance) {
