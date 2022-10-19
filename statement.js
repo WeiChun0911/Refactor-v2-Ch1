7cf9c281885fefc6a53b52bb1e505163afff3d32
WillyLiu <willywcliu@kknow.com>
2022-10-20 00:04:38 +0800

    Extract amountFor function

--- statement.js
+++ statement.js
@@ -10,8 +10,22 @@ const statement = (invoice, plays) => {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID]
-        let thisAmount = 0
+        let thisAmount = amountFor(play, perf)
 
+        // 加入 volumn credit
+        volumeCredits += Math.max(perf.audience - 30, 0)
+        // 每5名喜劇觀眾可以額外獲得點數
+        if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
+        // 印出這筆訂單
+        result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`
+        totalAmount += thisAmount
+    }
+    result += `Amount owed is ${format(totalAmount / 100)}\n`
+    result += `You earned ${volumeCredits} credits\n`
+    return result
+
+    function amountFor(play, perf) {
+        let thisAmount = 0
         switch (play.type) {
             case 'tragedy': {
                 thisAmount = 40000
@@ -27,17 +41,8 @@ const statement = (invoice, plays) => {
             default:
                 throw new Error(`unknown type: ${play.type}`)
         }
-        // 加入 volumn credit
-        volumeCredits += Math.max(perf.audience - 30, 0)
-        // 每5名喜劇觀眾可以額外獲得點數
-        if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
-        // 印出這筆訂單
-        result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`
-        totalAmount += thisAmount
+        return thisAmount
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`
-    result += `You earned ${volumeCredits} credits\n`
-    return result
 }
 
 export default statement
