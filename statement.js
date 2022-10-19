08506b7d9dfcf8136418577e46838a4026f3019c
WillyLiu <willywcliu@kknow.com>
2022-10-20 00:32:57 +0800

    把 customer 移進去中間物件

--- statement.js
+++ statement.js
@@ -1,9 +1,10 @@
 const statement = (invoice, plays) => {
     const statementData = {}
+    statementData.customer = invoice.customer
     return renderPlainText(statementData, invoice, plays)
 }
 function renderPlainText(data, invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`
+    let result = `Statement for ${data.customer}\n`
 
     for (let perf of invoice.performances) {
         // 印出這筆訂單
