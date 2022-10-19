a1357e36be489f6ef958a3cb7e0bbce7bbeff230
WillyLiu <willywcliu@kknow.com>
2022-10-20 01:08:51 +0800

    新增 renderHtml & testcase

--- test/statement.test.js
+++ test/statement.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import statement from '../statement.js'
+import { statement, htmlStatement } from '../statement.js'
 
 describe('statement', () => {
     let playsJson = {
@@ -40,16 +40,16 @@ describe('statement', () => {
         expect(statement(invoicesJson[0], playsJson)).to.equal(expected)
     })
 
-    // it('should print a statement for multiple plays, single customer and multiple seats in html', () => {
-    //     let result = `<h1>Statement for BigCo</h1>\n`
-    //     result += '<table>\n'
-    //     result += `<tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n`
-    //     result += `  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n`
-    //     result += `  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n`
-    //     result += '</table>\n'
-    //     result += `<p>Amount owed is <em>$1,730.00</em></p>\n`
-    //     result += `<p>You earned <em>47</em> credits</p>\n`
+    it('should print a statement for multiple plays, single customer and multiple seats in html', () => {
+        let result = `<h1>Statement for BigCo</h1>\n`
+        result += '<table>\n'
+        result += `<tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n`
+        result += `  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n`
+        result += `  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n`
+        result += '</table>\n'
+        result += `<p>Amount owed is <em>$1,730.00</em></p>\n`
+        result += `<p>You earned <em>47</em> credits</p>\n`
 
-    //     expect(htmlStatement(invoicesJson[0], playsJson)).to.equal(result)
-    // })
+        expect(htmlStatement(invoicesJson[0], playsJson)).to.equal(result)
+    })
 })
