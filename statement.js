c5d21ca80d9f4f57044f5a752ec537924f48fc31
WillyLiu <willywcliu@kknow.com>
2022-10-20 00:26:04 +0800

    Slide Statement

--- statement.js
+++ statement.js
@@ -1,13 +1,11 @@
 const statement = (invoice, plays) => {
-    let totalAmount = 0
-
     let result = `Statement for ${invoice.customer}\n`
 
     for (let perf of invoice.performances) {
         // 印出這筆訂單
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
     }
-
+    let totalAmount = 0
     for (let perf of invoice.performances) {
         totalAmount += amountFor(perf)
     }
