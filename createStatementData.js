9141d82593c27f55b05a6576ce32cdee9ceff6ae
WillyLiu <willywcliu@kknow.com>
2022-10-20 16:39:28 +0800

    把 volumnCredits 也移進去 calculator

--- createStatementData.js
+++ createStatementData.js
@@ -22,6 +22,13 @@ class PerformanceCalculator {
         }
         return result
     }
+
+    get volumeCredits() {
+        let result = 0
+        result += Math.max(this.performance.audience - 30, 0)
+        if (this.play.type === 'comedy') result += Math.floor(this.performance.audience / 5)
+        return result
+    }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -44,17 +51,10 @@ export default function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance)
         result.play = calculator.play
         result.amount = calculator.amount
-        result.volumeCredits = volumeCreditsFor(result)
+        result.volumeCredits = calculator.volumeCredits
         return result
     }
     function playFor(perf) {
         return plays[perf.playID]
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0
-        result += Math.max(aPerformance.audience - 30, 0)
-        if (aPerformance.play.type === 'comedy') result += Math.floor(aPerformance.audience / 5)
-        return result
     }
 }
